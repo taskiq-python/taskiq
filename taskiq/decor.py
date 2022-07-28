7db2e2fcc3f3cf2b2618831cc1dcf6142a0b2e46
Pavel Kirilin <win10@list.ru>
2022-07-28 14:13:22 +0400

    Removed unused files.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- taskiq/decor.py
+++
@@ -1,9 +0,0 @@
-import logging
-from typing import TypeVar
-
-from typing_extensions import ParamSpec
-
-logger = logging.getLogger("taskiq")
-
-_FuncParams = ParamSpec("_FuncParams")
-_ReturnType = TypeVar("_ReturnType")
--- taskiq/exceptions.py
+++ taskiq/exceptions.py
@@ -2,15 +2,5 @@ class TaskiqError(Exception):
     """Base exception for all errors."""
 
 
-class TaskiqBrokerIsNotSetError(TaskiqError):
-    """
-    Someone tried to access non existing broker.
-
-    This exception is thrown when you
-    call `kiq()` method before calling `set_broker`
-    function.
-    """
-
-
 class TaskiqResultTimeoutError(TaskiqError):
     """Waiting for task results has timed out."""
