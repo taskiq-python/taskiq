24c620f2830af0737f570ee21be26d7c17cbd821
Pavel Kirilin <win10@list.ru>
2022-08-24 21:47:36 +0400

    Added gather function.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- taskiq/__init__.py
+++ taskiq/__init__.py
@@ -7,11 +7,13 @@ from taskiq.brokers.inmemory_broker import InMemoryBroker
 from taskiq.brokers.shared_broker import async_shared_broker
 from taskiq.brokers.zmq_broker import ZeroMQBroker
 from taskiq.exceptions import TaskiqError
+from taskiq.funcs import gather
 from taskiq.message import BrokerMessage, TaskiqMessage
 from taskiq.result import TaskiqResult
 from taskiq.task import AsyncTaskiqTask
 
 __all__ = [
+    "gather",
     "AsyncBroker",
     "TaskiqError",
     "TaskiqResult",
