9efb46ee852b0bc492958b582e2259cef8a6b0be
Pavel Kirilin <win10@list.ru>
2022-09-07 17:03:15 +0400

    Added docs. (#45)
    Signed-off-by: Pavel Kirilin <win10@list.ru>

---
+++ docs/examples/introduction/inmemory_run.py
@@ -0,0 +1,27 @@
+# broker.py
+import asyncio
+
+from taskiq import InMemoryBroker
+
+broker = InMemoryBroker()
+
+
+@broker.task
+async def add_one(value: int) -> int:
+    return value + 1
+
+
+async def main() -> None:
+    # Send the task to the broker.
+    task = await add_one.kiq(1)
+    # Wait for the result.
+    result = await task.wait_result(timeout=2)
+    print(f"Task execution took: {result.execution_time} seconds.")
+    if not result.is_err:
+        print(f"Returned value: {result.return_value}")
+    else:
+        print("Error found while executing task.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- taskiq/__init__.py
+++ taskiq/__init__.py
@@ -6,6 +6,7 @@ from taskiq.abc.result_backend import AsyncResultBackend
 from taskiq.brokers.inmemory_broker import InMemoryBroker
 from taskiq.brokers.shared_broker import async_shared_broker
 from taskiq.brokers.zmq_broker import ZeroMQBroker
+from taskiq.context import Context
 from taskiq.exceptions import TaskiqError
 from taskiq.funcs import gather
 from taskiq.message import BrokerMessage, TaskiqMessage
@@ -14,6 +15,7 @@ from taskiq.task import AsyncTaskiqTask
 
 __all__ = [
     "gather",
+    "Context",
     "AsyncBroker",
     "TaskiqError",
     "TaskiqResult",
