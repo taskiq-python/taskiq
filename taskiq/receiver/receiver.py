bf162ad672177f5fe028dea16341201f96b29e00
Anton <myprojectorterrypratchett@gmail.com>
2023-05-10 20:16:54 +0300

    feat: receiver max_prefetch

--- taskiq/receiver/receiver.py
+++ taskiq/receiver/receiver.py
@@ -3,8 +3,9 @@ import inspect
 from concurrent.futures import Executor
 from logging import getLogger
 from time import time
-from typing import Any, Callable, Dict, Optional, get_type_hints
+from typing import Any, Callable, Dict, Optional, Set, get_type_hints
 
+import anyio
 from taskiq_dependencies import DependencyGraph
 
 from taskiq.abc.broker import AsyncBroker
@@ -17,6 +18,7 @@ from taskiq.state import TaskiqState
 from taskiq.utils import maybe_awaitable
 
 logger = getLogger(__name__)
+QUEUE_DONE = object()
 
 
 def _run_sync(target: Callable[..., Any], message: TaskiqMessage) -> Any:
@@ -36,12 +38,13 @@ def _run_sync(target: Callable[..., Any], message: TaskiqMessage) -> Any:
 class Receiver:
     """Class that uses as a callback handler."""
 
-    def __init__(
+    def __init__(  # noqa: WPS211
         self,
         broker: AsyncBroker,
         executor: Optional[Executor] = None,
         validate_params: bool = True,
         max_async_tasks: "Optional[int]" = None,
+        max_prefetch: int = 0,
     ) -> None:
         self.broker = broker
         self.executor = executor
@@ -61,6 +64,7 @@ class Receiver:
                 "Setting unlimited number of async tasks "
                 + "can result in undefined behavior",
             )
+        self.sem_prefetch = asyncio.Semaphore(max_prefetch)
 
     async def callback(  # noqa: C901, WPS213
         self,
@@ -230,7 +234,7 @@ class Receiver:
 
         return result
 
-    async def listen(self) -> None:  # pragma: no cover
+    async def listen(self) -> None:  # pragma: no cover # noqa:
         """
         This function iterates over tasks asynchronously.
 
@@ -239,7 +243,8 @@ class Receiver:
         """
         await self.broker.startup()
         logger.info("Listening started.")
-        tasks = set()
+        tasks: Set[asyncio.Task[Any]] = set()
+        queue: asyncio.Queue[bytes] = asyncio.Queue()
 
         def task_cb(task: "asyncio.Task[Any]") -> None:
             """
@@ -255,16 +260,49 @@ class Receiver:
             if self.sem is not None:
                 self.sem.release()
 
-        async for message in self.broker.listen():
-            # Waits for semaphore to be released.
-            if self.sem is not None:
-                await self.sem.acquire()
-            task = asyncio.create_task(self.callback(message=message, raise_err=False))
-            tasks.add(task)
+        async def runner() -> None:
+            """Run tasks."""
+            while True:
+                # Waits for semaphore to be released.
+                if self.sem is not None:
+                    await self.sem.acquire()
 
-            # We want the task to remove itself from the set when it's done.
-            #
-            # Because python's GC can silently cancel task
-            # and it considered to be Hisenbug.
-            # https://textual.textualize.io/blog/2023/02/11/the-heisenbug-lurking-in-your-async-code/
-            task.add_done_callback(task_cb)
+                self.sem_prefetch.release()
+                message = await queue.get()
+                if message is QUEUE_DONE:
+                    break
+
+                task = asyncio.create_task(
+                    self.callback(message=message, raise_err=False),
+                )
+                tasks.add(task)
+
+                # We want the task to remove itself from the set when it's done.
+                #
+                # Because python's GC can silently cancel task
+                # and it considered to be Hisenbug.
+                # https://textual.textualize.io/blog/2023/02/11/the-heisenbug-lurking-in-your-async-code/
+                task.add_done_callback(task_cb)
+
+        async with anyio.create_task_group() as gr:
+            gr.start_soon(self.prefetcher, queue)
+            gr.start_soon(runner, queue)
+
+    async def prefetcher(self, queue: "asyncio.Queue[Any]") -> None:
+        """
+        Prefetch tasks data.
+
+        :param queue: queue for prefetched data.
+        """
+        iterator = self.broker.listen()
+
+        while True:
+            try:
+                await self.sem_prefetch.acquire()
+                message = await iterator.__anext__()  # noqa: WPS609
+                await queue.put(message)
+
+            except StopAsyncIteration:
+                break
+
+        await queue.put(QUEUE_DONE)
