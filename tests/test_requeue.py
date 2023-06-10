8afb1c9424ffc23c781ff8a54f4c63709b47b472
Pavel Kirilin <win10@list.ru>
2023-06-10 21:28:20 +0400

    Added context methods to reject and requeue. (#152)

---
+++ tests/test_requeue.py
@@ -0,0 +1,22 @@
+import pytest
+
+from taskiq import Context, InMemoryBroker, TaskiqDepends
+
+
+@pytest.mark.anyio
+async def test_requeue() -> None:
+    broker = InMemoryBroker()
+
+    runs_count = 0
+
+    @broker.task
+    async def task(context: Context = TaskiqDepends()) -> None:
+        nonlocal runs_count
+        runs_count += 1
+        if runs_count < 2:
+            await context.requeue()
+
+    kicked = await task.kiq()
+    await kicked.wait_result()
+
+    assert runs_count == 2
