d9bca890d96d554d8f6ebb5a8ec7289e1ec8a92b
Pavel Kirilin <win10@list.ru>
2023-06-10 22:25:43 +0400

    Added exception catches from dependencies. (#153)

--- tests/test_requeue.py
+++ tests/test_requeue.py
@@ -20,3 +20,25 @@ async def test_requeue() -> None:
     await kicked.wait_result()
 
     assert runs_count == 2
+
+
+@pytest.mark.anyio
+async def test_requeue_from_dependency() -> None:
+    broker = InMemoryBroker()
+
+    runs_count = 0
+
+    async def dep_func(context: Context = TaskiqDepends()) -> None:
+        nonlocal runs_count
+        runs_count += 1
+        if runs_count < 2:
+            await context.requeue()
+
+    @broker.task
+    async def task(_: None = TaskiqDepends(dep_func)) -> None:
+        return None
+
+    kicked = await task.kiq()
+    await kicked.wait_result()
+
+    assert runs_count == 2
