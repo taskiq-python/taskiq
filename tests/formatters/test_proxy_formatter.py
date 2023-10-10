c1986242df3bbad0c12b15bef7b60335539ba237
Andrew Svetlov <andrew.svetlov@gmail.com>
2023-10-10 16:29:06 +0200

    Implement taskiq serializer

---
+++ tests/formatters/test_proxy_formatter.py
@@ -0,0 +1,47 @@
+import pytest
+
+from taskiq.brokers.inmemory_broker import InMemoryBroker
+from taskiq.message import BrokerMessage, TaskiqMessage
+
+
+@pytest.mark.anyio
+async def test_proxy_dumps() -> None:
+    # uses json serializer by default
+    broker = InMemoryBroker()
+    msg = TaskiqMessage(
+        task_id="task-id",
+        task_name="task.name",
+        labels={"label1": 1, "label2": "text"},
+        args=[1, "a"],
+        kwargs={"p1": "v1"},
+    )
+    expected = BrokerMessage(
+        task_id="task-id",
+        task_name="task.name",
+        message=(
+            b'{"task_id": "task-id", "task_name": "task.name", '
+            b'"labels": {"label1": 1, "label2": "text"}, '
+            b'"args": [1, "a"], "kwargs": {"p1": "v1"}}'
+        ),
+        labels={"label1": 1, "label2": "text"},
+    )
+    assert broker.formatter.dumps(msg) == expected
+
+
+@pytest.mark.anyio
+async def test_proxy_loads() -> None:
+    # uses json serializer by default
+    broker = InMemoryBroker()
+    msg = (
+        b'{"task_id":"task-id","task_name":"task.name",'
+        b'"labels":{"label1":1,"label2":"text"},'
+        b'"args":[1,"a"],"kwargs":{"p1":"v1"}}'
+    )
+    expected = TaskiqMessage(
+        task_id="task-id",
+        task_name="task.name",
+        labels={"label1": 1, "label2": "text"},
+        args=[1, "a"],
+        kwargs={"p1": "v1"},
+    )
+    assert broker.formatter.loads(msg) == expected
