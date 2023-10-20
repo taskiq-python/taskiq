e944e9e5828b07b7f49b0b3578a31fd4ceab3206
Pavel Kirilin <win10@list.ru>
2023-10-21 01:52:13 +0400

    Added pickle serializer.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- taskiq/serializers/__init__.py
+++ taskiq/serializers/__init__.py
@@ -3,10 +3,12 @@ from .cbor_serializer import CBORSerializer
 from .json_serializer import JSONSerializer
 from .msgpack_serializer import MSGPackSerializer
 from .orjson_serializer import ORJSONSerializer
+from .pickle import PickleSerializer
 
 __all__ = [
     "JSONSerializer",
     "ORJSONSerializer",
     "MSGPackSerializer",
     "CBORSerializer",
+    "PickleSerializer",
 ]
