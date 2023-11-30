a49d9d4d3f0883bb7500beebb7c324c9bd648bd3
Pavel Kirilin <win10@list.ru>
2023-11-30 22:13:36 +0100

    Added ack config.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- taskiq/acks.py
+++ taskiq/acks.py
@@ -1,6 +1,23 @@
+import enum
 from typing import Awaitable, Callable, Union
 
 from pydantic import BaseModel
+
+
+@enum.unique
+class AcknowledgeType(enum.StrEnum):
+    """Enum with possible acknowledge times."""
+
+    # The message is acknowledged right when it's received,
+    # before it's executed.
+    WHEN_RECEIVED = enum.auto()
+    # This option means that the message will be
+    # acknowledged right after it's executed.
+    WHEN_EXECUTED = enum.auto()
+    # This option means that the message will be
+    # acknowledged when the task will be saved
+    # only after it's saved in the result backend.
+    WHEN_SAVED = enum.auto()
 
 
 class AckableMessage(BaseModel):
